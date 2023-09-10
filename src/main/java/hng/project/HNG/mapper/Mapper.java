de4750cc2c5ecfa3805cdad054855695e90e801b
whitewizardd <o.samuel@native.semicolon.africa>
2023-09-10 21:54:16 +0100

    updatted

--- src/main/java/hng/project/HNG/mapper/Mapper.java
+++ src/main/java/hng/project/HNG/mapper/Mapper.java
@@ -2,22 +2,36 @@ package hng.project.HNG.mapper;
 
 import hng.project.HNG.data.models.Slack;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.logging.SimpleFormatter;
+
 public class Mapper {
     public static Slack map(String slack_name, String track){
         Slack slack = new Slack();
         slack.setSlack_name(slack_name);
         slack.setTrack(track);
         slack.setStatus_code(200);
-        slack.setGithub_file_url("");
+        slack.setGithub_file_url("https://github.com/whitewizardd/HNG_Project/blob/main/src/main/java/hng/project/HNG/controllers/HngController.java");
         slack.setGithub_repo_url("https://github.com/whitewizardd/HNG_Project.git");
         slack.setCurrent_day(getDay());
         slack.setUtc_time(getUtcTime());
         return slack;
     }
     private static String getDay(){
-        return null;
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        DateFormat formatter = new SimpleDateFormat("EEEE");
+        return formatter.format(date);
     }
     private static String getUtcTime(){
-        return null;
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return formatter.format(date);
     }
 }
